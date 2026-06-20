7b0f238a06fda7004358120c0d121259ac5a1b8f
Aniket <aniketsolanke1404@gmail.com>
2026-06-21 01:14:19 +0530

    Fix Vercel build

--- my-project (2)/my-project/my-project/src/Services/JobService.ts
+++ my-project (2)/my-project/my-project/src/Services/JobService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080";
+const API_URL = "https://jobhook-46y3.onrender.com";
 
 const apiClient = axios.create({
     baseURL: API_URL,
